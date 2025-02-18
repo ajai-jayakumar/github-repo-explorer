828583f30a70d9ca89a188eb183ad63168fae47b
ajai-jayakumar <ajai199@gmail.com>
2025-02-18 14:12:12 +0100

    chore: add @tanstack/react-query with dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import Home from './pages/Home';
+import { ReactQueryProvider } from './providers/ReactQueryProvider';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ReactQueryProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </ReactQueryProvider>
   );
 }
